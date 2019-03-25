77b94d154b2570eba6d05784a49647101163e132
Alex Lee <hyunho98@gmail.com>
2019-03-25 02:27:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function whisper(string) {
 function logShout(string) {
   return console.log(string.toUpperCase())
 }
+
+function logWhisper (string) {
+  return console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === lowercase) {
+    return "I can't hear you!"
+  }
+  else if (string === uppercase) {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
